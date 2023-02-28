c0a8532a0183c034e41b5bc1062adbc894e73c51
TomTomCoffey <tomcoffey1017@yahoo.com>
2023-02-27 21:04:00 -0500

    Adding in the footer to view Needs serious editing

--- Exercise-project/client/src/App.vue
+++ Exercise-project/client/src/App.vue
@@ -2,18 +2,24 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import NavBar from '@/components/NavBar.vue'
+import Footer from '@/components/Footer.vue'
 
 </script>
 
 <template>
   <NavBar />
- 
+
+
+
+
   
   <div class="container">
     <RouterView />
   </div>
   
   <RouterView />
+
+  <Footer />
 </template>
 
 <style scoped>
---
+++ Exercise-project/client/src/components/Footer.vue
@@ -0,0 +1,31 @@
+<script setup lang= "ts">
+import { RouterLink, RouterView } from 'vue-router';
+
+const isMenuActive = false;
+
+</script>
+
+<template>
+
+<footer class="footer">
+        <div class="content has-text-centered">
+          <p>
+            <strong>Bulma</strong> by <a href="">Thomas Coffey</a>. Exercise Project
+            <a href="">Suny New Paltz</a>
+          
+          </p>
+        </div>
+      </footer>
+
+</template>
+
+<style scoped>
+
+.footer{
+    background-color: #00d1b2;
+    color: white;
+    padding: 1rem;
+    margin-top: 1rem;
+}
+</style>
+
