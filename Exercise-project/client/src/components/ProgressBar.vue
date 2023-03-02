355f2908f2fcbadd9aa3071ed23541b4f0315878
TomTomCoffey <tomcoffey1017@yahoo.com>
2023-03-01 19:33:32 -0500

    Adding components

---
+++ Exercise-project/client/src/components/ProgressBar.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+    props: ['msg']
+    
+</script>
+
+<template>
+
+<article class="message">
+              <div class="message-header">
+                <p>Your exersise progress!</p>
+                <button class="delete" aria-label="delete"></button>
+              </div>
+              <div class="message-body">
+               <p> Track your progress and fill the bar to meet your goals! </p>
+                <progress class="progress is-success" value="60" max="100">60%</progress>
+              </div>
+            </article>
+</template>
+
+
+<style scoped>
+
+</style>
--- Exercise-project/client/src/views/AboutView.vue
+++ Exercise-project/client/src/views/AboutView.vue
@@ -1,45 +1,30 @@
+<script setup lang="ts">
+  import { RouterLink, RouterView } from 'vue-router'
+  import aboutMsg from '@/components/AboutMessage.vue'
+  import ProgressBar from '@/components/ProgressBar.vue'
+
+  </script>
 <template>
 
 <div class="container">
    <div class="columns">
       <div class="column">
         <div class="notification is-link is-light">
-           <article class="message is-medium">
-             <div class="message-header">
-               <p>About the App</p>
-               <button class="delete is-medium" aria-label="delete"></button>
-             </div>
-  <div class="message-body">
-   <p>
-    This app is designed to help you record your exercise 
-    progess and keep track of your goals. By recording your workouts,
-     you can see your progress and see how far you have come. 
-     Sign up to the app and being a better stronger you! 
-          </p>
-        </div>
-      </article>
+     <aboutMsg />
+     <ProgressBar />
     </div>
-
-    <article class="message">
-              <div class="message-header">
-                <p>Your exersise progress!</p>
-                <button class="delete" aria-label="delete"></button>
-              </div>
-              <div class="message-body">
-               <p> This is your current workout progress for today! </p>
-                <progress class="progress is-success" value="60" max="100">60%</progress>
-              </div>
-            </article>
-  </div>
-    <div class="column">
+      </div>
+ 
+     <div class="column">
     Second column
-    </div>
+    </div> 
+  </div>
 </div>
 
 
 
   
-</div>
+
    
 
 </template>
