9c39e03af03ab7e48238be30cefc523a81be7441
TomTomCoffey <tomcoffey1017@yahoo.com>
2023-03-05 15:26:20 -0500

    Working on making components and how should i be placing them in views

---
+++ Exercise-project/client/src/components/RunStats.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <div class="box has-text-success summary">
+            <h2 class="title">Today</h2>
+            <div class="colums is-multiline">
+                <div class="column is-half">
+                    <h3 class="value">0.0</h3>
+                        Distance
+
+                </div>
+                <div class="column is-half">
+                    <h3 class="value">0.00</h3>
+                        Duration
+
+                </div>
+                
+                <div class="column is-half">
+                    <h3 class="value">-</h3>
+                        Ave Pace
+
+                </div>
+
+                <div class="column is-half">
+                    <h3 class="value">0.0</h3>
+                        Calories
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+
+
+<style scoped>
+
+.summary{
+    background-color: #0c5e44;
+    color: white;
+    padding: 1rem;
+    
+    
+}
+
+
+
+
+</style>
