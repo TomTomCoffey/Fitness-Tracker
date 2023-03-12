0cf33690005bae7d892f4012ab7ff66fe1ecb0cd
TomTomCoffey <tomcoffey1017@yahoo.com>
2023-03-12 13:30:05 -0400

    Login Now Drops to choose how to log in as.

--- Exercise-project/client/src/components/LoginBadge.vue
+++ Exercise-project/client/src/components/LoginBadge.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
  import { ref } from 'vue';
  import { useSession, login } from '@/model/session';
+ import SignUpBadge from './SignUpBadge.vue';
 
 
  const session = useSession();
@@ -13,27 +14,61 @@
                  (<a @click=" session.user = null">logout</a>)
              </div>
 
+                
              <div class="navbar-item" v-else>
-                <div class="dropdown is active"></div>
-                 <a class="button" @click="login(2)">
-                     <span class="icon">
-                         <i class="fas fa-user"></i>
-                     </span>
-                     <strong>Login</strong>
-                 </a>
-                 <a class="button is-info">
-                     <span class="icon">
-                         <i class="fas fa-user-plus"></i>
-                     </span>
-                     <strong>Sign up</strong>
-                 </a>
-             </div>
+                   <div class="navbar-item has-dropdown is-hoverable">
+                    <a class="navbar-link">
+                        <strong>Log in</strong>
+                    </a>
+                    <div class="navbar-dropdown">
+                    <a class="button" @click="login(1)">
+                        <strong>Tom</strong>
+                    </a>
+                    <a class="button" @click="login(2)">
+                        <strong>Zoey</strong>
+                    </a>
+                    <a class="button" @click="login(3)">
+                        <strong>Sally</strong>
+                    </a>
+                    </div>
              
+                    </div> 
+                
+              <SignUpBadge/>
+
+
+                </div>
+
+
 
 
 
  </template>
 
  <style scoped>
+    .navbar-item{
+        color: #0c5e44;
+    }
+    .navbar-link{
+        color: #0c5e44;
+    }
+    .navbar-dropdown{
+        background-color: #0c5e44;
+        color: white;
+    }
+    .button{
+        background-color: #0c5e44;
+        color: white;
+        margin: 0.5rem;
+    }
+    .button:hover{
+        background-color: #0c5e44;
+        color: white;
+        margin: 0.5rem;
+    }
+    
+
+
+    
 
  </style>
---
+++ Exercise-project/client/src/components/SignUpBadge.vue
@@ -0,0 +1,26 @@
+
+<script setup lang="ts">
+
+</script>
+
+
+
+<template>
+
+
+    <div>
+        <a class="button is-info">
+                     <span class="icon">
+                         <i class="fas fa-user-plus"></i>
+                     </span>
+                     <strong>Sign up</strong>
+                 </a>
+
+    </div>
+</template>
+
+
+
+<style scoped>
+
+</style>
