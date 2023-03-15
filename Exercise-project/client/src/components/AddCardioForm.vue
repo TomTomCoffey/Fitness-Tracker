f389b138f13abec624c9cfafbc7841de45fc5fec
TomTomCoffey <tomcoffey1017@yahoo.com>
2023-03-14 20:03:37 -0400

    Added addcardio component

---
+++ Exercise-project/client/src/components/AddCardioForm.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+
+</script>
+
+
+
+<template>
+    <div>
+        <div class="modal is-active">
+         <div class="model-background"></div>
+          <div class="modal-card">
+            <header class="modal-card-head">
+              <p class="modal-card-title">Add Run</p>
+              <button class="delete" aria-label="close"></button>
+            </header>
+            <section class="modal-card-body">
+                <div class="field">
+                    <label class="label">Distance (in miles)</label>
+                    <div class="control">
+                    <input class="input" type="text" placeholder="Workout Name">
+                    </div>
+                </div>
+          
+                <div class="field">
+                    <label class="label">Duration (in mins)</label>
+                    <div class="control">
+                    <input class="input" type="text" placeholder="Workout Name">
+                    </div>
+                </div>
+            </section>
+            <footer class="modal-card-foot">
+              <button class="button is-success">Save changes</button>
+              <button class="button">Cancel</button>
+            </footer>
+        </div>
+
+        </div>
+
+    </div>
+</template>
+
+
+
+<style scoped>
+
+</style>
