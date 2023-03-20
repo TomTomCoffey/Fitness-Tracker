5d2e50dc18690a73137d75deb7ceadb69811aac1
TomTomCoffey <tomcoffey1017@yahoo.com>
2023-03-19 22:44:13 -0400

    Admin view styled

--- Exercise-project/client/src/views/AdminView.vue
+++ Exercise-project/client/src/views/AdminView.vue
@@ -5,8 +5,11 @@ import AdminBox from '@/components/AdminBox.vue';
 
 
 <template>
+    <div class="column is half"></div>
     <div>
+        <div class="container">
         <AdminBox  />
+        </div>
        
     </div>
 </template>
@@ -14,5 +17,23 @@ import AdminBox from '@/components/AdminBox.vue';
 
 
 <style scoped>
+
+.container{
+    display: center;
+    padding: 0;
+    margin: 5;
+    background-color: #def2f1;
+    color: #2b7a78;
+    margin-right: 5%;
+    margin-left: 5%;
+    margin-bottom: -1rem;
+    padding: 1rem;
+    padding-top: 2rem;
+    padding-bottom: 2rem;
+    border-radius: 0.5rem;
+    box-shadow: 0 0 10px 0 rgba(0,0,0,0.2);
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+}
 
 </style>
