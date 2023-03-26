eec8d46bfcd3806b19ddd2062de1b3bebee763d1
TomTomCoffey <tomcoffey1017@yahoo.com>
2023-03-26 13:37:11 -0400

    Working on api's getting rid of old HTML files

--- Exercise-project/server/controllers/sessions.js
+++ Exercise-project/server/controllers/sessions.js
@@ -1,26 +1,56 @@
 const express = require('express');
+const model = require('../models/sessions');
 const router = express.Router();
 
 router
     .get('/', (req, res) => {
-        res.send({
-            items: [
-                {
-                    id: 1,
-                    name: 'Product 1'
-                },
-            ]
-        })
+        const list = model.getSession();
+        res.send(list)
     })
+
+    .get('/search/:q', (req, res) => {
+        const term = req.params.q;
+        console.log({ term });
+        const list = model.searchSessions(term);
+        res.send(list);
+    })
+
     .get('/:id', (req, res) => {
-        
+        const id = +req.params.id;
+        const product = model.getSessionById(id);
+        res.send(product);
     })
+
     .post('/', (req, res) => {
-        res.send('Thank you for your order')
+        const product = req.body;
+
+        console.log({ product });
+        console.log( req.query );
+        console.log( req.params );
+        console.log( req.headers );
+
+        model.addProduct(user);
+        res.send(user);
     })
+
     .patch('/:id', (req, res) => {
+        const product = req.body;
+        model.updateProduct(user);
+        res.send(user);
     })
+
     .delete('/:id', (req, res) => {
+        const id = +req.params.id;
+        model.deleteProduct(id);
+        res.send({id});
     })
 
 module.exports = router;
+
+/*  Ways to pass information to the server:
+    1. Query String Parameters
+    2. Route Parameters
+    3. Request Body
+    4. Request Headers
+    5. Cookies
+*/
--- Exercise-project/server/index.js
+++ Exercise-project/server/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
-const path = require('path')
-///const products = require('./controllers/products')
+const path = require('path');
+//const jokes = require('./controllers/jokes');
+//const products = require('./controllers/products')
+const sessions = require('./controllers/sessions')
 const app = express()
 
 const hostname = '127.0.0.1';
@@ -9,18 +11,32 @@ const port = process.env.PORT || 3000;
 // Middleware
 app
     .use(express.json())
-    .use(express.static(path.join(__dirname, '../client/dist')))
+    .use(express.static(path.join(__dirname, '../client')))
+    .use((req, res, next) => {
+        console.log(`${req.method} ${req.url}`);
+        next();
+    })
+
 
 
 // Actions
 app
-    .get('/', (req, res) => {
+    .get('/api/v1/', (req, res) => {
         res.send('Hello World! From Express')
     })
-   /// .use('/products', products)
+    .use('/api/v1/products', sessions)
+   // .use('/api/v1/jokes', jokes)
+
+// Catch all
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+})
 
 
+console.log('1: About to start server')
 
 app.listen(port, () => 
-  console.log(`Server running at http://${hostname}:${port}/`)
+  console.log(`2: Server running at http://${hostname}:${port}/`)
 );
+
+console.log('3: Asked server to start')
