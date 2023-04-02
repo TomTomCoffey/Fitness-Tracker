f9583209b48818e7a15b785d39d138c0acbac57d
TomTomCoffey <tomcoffey1017@yahoo.com>
2023-04-02 12:32:10 -0400

    Data envelope is in my fetch

--- Exercise-project/client/src/model/myFetch.ts
+++ Exercise-project/client/src/model/myFetch.ts
@@ -1,9 +1,21 @@
 const API_URL = 'http://localhost:3000/api/v1/';
 
-export function rest(url: string){
-    return fetch(url).then(res => res.json());
+export async function rest(url: string){
+    const res = await fetch(url);
+    return await res.json();
 }
 
 export function api(url: string){
     return rest(API_URL + url);
+}
+
+
+export type DataEnvelope<T> = {
+    data: T,
+    isSuccess: boolean,
+    error?: string,
+}
+
+export type DataListEnvelope<T> = DataEnvelope<T[]> & {
+    total: number,
 }
